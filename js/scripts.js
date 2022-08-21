2253b42d7bf42ec7ac669409b6c15b147acdf837
FacuBnz <facu.j.benitez@gmail.com>
2022-08-21 05:13:55 -0300

    cambios generales

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,46 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+(function () {
+    'use strict'
+    let form = document.getElementById("formConsult");
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+        let name = document.getElementById("nameForm").value;
+        let email = document.getElementById("emailForm").value;
+        let message = document.getElementById("messageForm").value;
+        if((name != null && name != "") && (email != null && email != "") && (message != null && message != "")){
+            //console.log(name, email, message);
+
+            let req = {
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            }
+            const options = {
+                method: 'POST',
+                body: JSON.stringify(req),
+                headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+            }
+
+            fetch('https://jsonplaceholder.typicode.com/posts', options)
+            .then(data => {
+                if (!data.ok) {
+                    throw Error(data.status);
+                }
+                    return data.json();
+                }).then(update => {
+                    console.log(update);
+                    let alert = document.getElementById("alr");
+                    alert.style.display = "block";
+                }).catch(e => {
+                    console.log(e);
+                });
+        }
+    }, false)
+
+  })()
